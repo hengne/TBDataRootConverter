08d3793a1bfbdbab786768280e9e64834c231ed1
Hengne Li <Hengne.Li@CERN.CH>
2014-03-13 23:07:39 +0100

    2nd update

--- convert1.cc
+++ convert1.cc
@@ -27,29 +27,37 @@ int main(int argc, char* argv[])
   std::ifstream infile(argv[1]);
   TFile* fout = new TFile(argv[2], "recreate");
 
-  int time_stamp, trig_number, board, channel, ndata;
-  double data[600];
+  char date[100], time[100], ampm[10];
+  char time_stamp[300];
+  int event, channel, ndata; 
+  int data[600];
 
   TTree* tree = new TTree("tree", "tree");
-  tree->Branch("time_stamp", &time_stamp, "time_stamp/I");
-  tree->Branch("trig_number", &trig_number, "trig_number/I");
-  tree->Branch("board", &board, "board/I");
-  tree->Branch("channel", &channel, "channle/I");
+  tree->Branch("time_stamp", &time_stamp, "time_stamp/C");
+  tree->Branch("event", &event, "event/I");
+  //tree->Branch("trig_number", &trig_number, "trig_number/I");
+  //tree->Branch("board", &board, "board/I");
+  tree->Branch("channel", &channel, "channel/I");
   tree->Branch("ndata", &ndata, "ndata/I");
-  tree->Branch("data", data, "data[ndata]/D");
+  tree->Branch("data", data, "data[ndata]/I");
 
   std::string line;
-
+  
   if (infile.is_open())
   {
+    event=0;
     while (getline(infile,line))
     {
+      event++;
       std::stringstream sline(line);
-      sline >> time_stamp >> trig_number >> board >> channel;
-      ndata=13;
-      for (int idata=0; idata<ndata; idata++)
-      {  
-        sline >> data[idata];
+      sline >> date >> time >> ampm >> channel;
+      sprintf(time_stamp, "%s %s %s", date, time, ampm);
+      ndata=0;
+      int dd;
+      while ( sline >> dd ) 
+      {
+        data[ndata] = dd;
+        ndata++;
       }
 
       tree->Fill();
